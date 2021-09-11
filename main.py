de5b31d46a3f471e3e89d6cb74345fce89964f94
H-Fujita <xxmade.in.moonxx@gmail.com>
2021-09-11 21:56:30 +0900

    Wave書き出し対応
    残：複数ファイルを結合する

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ import os
 import numbers
 import random
 
+import wave
+import numpy as np
+from matplotlib import pylab as plt
+import struct
+
+a = 1     #振幅
+fs = 8000 #サンプリング周波数
+f0 = 440  #周波数
+sec = 1   #秒
+# swav=[]
+wav=[]
+# for n in np.arange(fs * sec):
+#     #サイン波を生成
+#     s = a * np.sin(2.0 * np.pi * f0 * n / fs)
+#     swav.append(s)
+# #サイン波を表示
+# plt.plot(swav[0:100])
+# plt.show()
+# #サイン波を-32768から32767の整数値に変換(signed 16bit pcm)
+# swav = [int(x * 32767.0) for x in swav]
+# #バイナリ化
+# binwave = struct.pack("h" * len(swav), *swav)
+# #サイン波をwavファイルとして書き出し
+# w = wave.Wave_write("output.wav")
+# p = (1, 2, 8000, len(binwave), 'NONE', 'not compressed')
+# w.setparams(p)
+# w.writeframes(binwave)
+# w.close()
+
+
 # 対象画像読み込み
 
 PICNAME = "Fujita.png"
@@ -21,6 +51,8 @@ boxToY = height #対象範囲終了位置 Y座標
 
 imgBox = [[] for _ in imgsize]
 rgbhsv = []
+octerve = []
+
 
 for i in imgsize:
     imgBox[i] = img[boxFromY: boxToY, boxFromX: boxToX]
@@ -43,13 +75,28 @@ for i in imgsize:
     s = imgBoxHsv[i].T[1].flatten().mean()
     v = imgBoxHsv[i].T[2].flatten().mean()
     
-    sv= abs(s-v)
-    print("sv: %.2f" % (sv))
-    octerve = abs(h-90)*s*v/500
-    # HSV平均値を出力
-    # uHeは[0,179], Saturationは[0,255]，Valueは[0,255]
-    
-    print("octerve: %.2f" % (octerve))
-
-    rgbhsv.append([r,g,b,h,s,v])
+    # sv= abs(s-v)
+    # print("sv: %.2f" % (sv))
+    culc = abs(h-90)*s*v/500
+    octerve.append(culc)
     boxToX = boxToX + 20 #対象範囲終了位置 X座標
+
+swav =[]
+for j in range(len(octerve)):   
+    print("octervej: %.2f" % octerve[j])
+        
+    for n in np.arange(fs * sec):
+        #サイン波を生成
+        s = a * np.sin(2.0 * np.pi * octerve[j] * (n+1)/ fs)
+        swav.append(s)
+    #サイン波を-32768から32767の整数値に変換(signed 16bit pcmへ)
+    swav = [int(x * 32767.0) for x in swav]
+    #バイナリ化
+    binwave = struct.pack("h" * len(swav), *swav)
+    #サイン波をwavファイルとして書き出し
+    w = wave.Wave_write("output%.2f.wav" % j)
+    p = (1, 2, 8000, len(binwave), 'NONE', 'not compressed')
+    w.setparams(p)
+    w.writeframes(binwave)
+    w.close()
+    swav = []
